4057cfd40ffc664a94143aba31a0093dcc97d1df
Taha Majlesi Pour <srtaha2022@gmail.com>
2024-10-04 19:37:17 -0700

    little update

--- js/Login.js
+++ js/Login.js
@@ -1,9 +1,37 @@
-let floating_email = document.getElementById(`floating_email`).value;
-let floating_password = document.getElementById(`floating_password`).value;
+let floating_username = document.getElementById(`floating_username`);
+let floating_password = document.getElementById(`floating_password`);
 let RememberPass = document.getElementById(`RememberPass`);
 let submitbtn = document.getElementById(`submitbtn`);
 let forgotpassBtn = document.getElementById(`forgotpassBtn`);
 
-function EmailValication() {}
+function GetData(e) {
+  fetch("../db.json")
+    .then((res) => {
+      res.json;
+    })
+    .then((data) => {
+      data.forEach((elem) => {
+        if (elem.UserName == e) {
+          return true;
+        }
+      });
+    });
+}
 
-submitbtn.addEventListener("click", EmailValication, true);
+function UsernameLength() {
+  if (floating_username.value.length < 6) {
+    floating_username.insertAdjacentHTML(
+      "afterend",
+      `<p class="text-white">UserName is too short !</p>`
+    );
+  } else if (GetData(floating_username) != true) {
+    floating_username.insertAdjacentHTML(
+      "afterend",
+      `<p class="text-white">Invalid UserName</p>`
+    );
+  }
+}
+
+function PostData() {}
+
+submitbtn.addEventListener("click", UsernameLength);
